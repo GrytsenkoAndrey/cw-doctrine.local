5072f39430850ee13a4284be995b8f126c9134f0
Andrey Grytsenko <andreyapg@gmail.com>
2022-11-03 14:20:04 +0200

    ArticleController 2022-11-03

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class ArticleController
+{
+    public function index(Request $request, Response $response)
+    {
+        $article = $this->ci->get('db')->find('App\Entity\Article', 1);
+
+        return $this->renderPage($response, 'article.html', [
+            'article' => $article
+        ]);
+    }
+}
