e1455c658b37c1637b8a60eaeab285254e522000
Andrey Grytsenko <andreyapg@gmail.com>
2022-11-07 14:32:52 +0200

    23 Doctrine Query Language 2022-11-07

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,6 +10,21 @@ class DefaultController
 {
     public function homepage(Request $request, Response $response)
     {
+        $dql = "SELECT a FROM App\Entity\Article a "
+            ."WHERE a.published <= CURRENT_TIMESTAMP() "
+            ."ORDER BY a.published DESC";
+
+        $query = $this->ci->get('db')
+            ->createQuery($dql);
+        $articles = $query->getResult();
+
+        return $this->renderPage($response, 'homepage.html', [
+            'article' => $articles
+        ]);
+    }
+
+    public function oldHomepage(Request $request, Response $response)
+    {
         $articles = $this->ci
             ->get('db')
             ->getRepository('App\Entity\Article')
